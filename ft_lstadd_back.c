ee1f7341af4b7bf14140552a91a2e06414735534
Butcher Sarai <bsarai@at-k3.msk.21-school.ru>
2022-06-29 09:16:22 +0300

    refactor lstadd_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -2,12 +2,7 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list	*last;
-
-	if (*lst)
-	{
-		last = ft_lstlast(*lst);
-		last->next = new;
-		new->next = NULL;
-	}
+	while (*lst)
+		lst = &((*lst)->next);
+	*lst = new;
 }
