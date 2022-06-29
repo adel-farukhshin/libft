255139c947d8a412125e11bcc691f7ace5ee4f4a
Butcher Sarai <bsarai@at-k3.msk.21-school.ru>
2022-06-29 08:14:12 +0300

    refactor lstsize: iterate not to prev - 1, but to prev

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -1,15 +1,23 @@
 #include "libft.h"
 
-int		ft_lstsize(t_list *lst)
+int	ft_lstsize(t_list *lst)
 {
 	int	i;
 
 	i = 0;
-	if (lst)
-		while (lst->next)
-		{
-			i++;
-			lst = lst->next;
-		}
+	while (lst)
+	{
+		lst = lst->next;
+		i++;
+	}
 	return (i);
 }
+
+// int	main()
+// {
+// 	t_list * l =  NULL;
+// 	ft_lstadd_front(&l, ft_lstnew((void*)1));
+// 	printf("%p\n", l->content);
+// 	printf("%d\n", ft_lstsize(l));
+
+// }
